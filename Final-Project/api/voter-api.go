ee2df507b1561b41d2e6b2a83a1da284970ea6af
Andrew Marku <andrewmarku@gmail.com>
2023-09-02 12:47:58 -0400

    add files from previous project

---
+++ Final-Project/api/voter-api.go
@@ -0,0 +1,120 @@
+package api
+
+import (
+	"Voter-Container/voter"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type VoterApi struct {
+	voterList *voter.VoterList
+}
+
+func NewVoterApi() *VoterApi {
+	return &VoterApi{
+		voter.NewVoterList(),
+	}
+}
+
+func (v *VoterApi) AddVoter(c *gin.Context) {
+	//v.voterList.Voters[voterID] = *voter.NewVoter(voterID, firstName, lastName)
+	var newVoter voter.Voter
+
+	if err := c.ShouldBindJSON(&newVoter); err != nil {
+		log.Println("Error binding JSON: ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	c.JSON(http.StatusOK, newVoter)
+}
+
+func (v *VoterApi) AddPoll(c *gin.Context, voterID, pollID uint) {
+	voter, err := v.voterList.GetVoter(voterID)
+	if err != nil {
+		log.Println("error getting voter ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	voter.AddPoll(pollID)
+}
+
+func (v *VoterApi) GetVoter(c *gin.Context, voterID uint) *voter.Voter {
+	voter, err := v.voterList.GetVoter(voterID)
+	if err != nil {
+		log.Println("error getting voter: " + err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	return voter
+}
+
+func (v *VoterApi) GetVoterJson(c *gin.Context, voterID uint) string {
+	voter, err := v.voterList.GetVoter(voterID)
+	if err != nil {
+		log.Println("error getting voter: " + err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	return voter.ToJson()
+}
+
+func (v *VoterApi) GetVoterList() *voter.VoterList {
+	return v.voterList
+}
+
+func (v *VoterApi) GetVoterListJson() string {
+	b, _ := json.Marshal(v.voterList)
+	return string(b)
+}
+
+func (v *VoterApi) ListAllVoters(c *gin.Context) {
+	c.JSON(http.StatusOK, v.voterList)
+}
+
+func (v *VoterApi) ListVoter(c *gin.Context) {
+	id := getIDFromContext(c, "id")
+	voter := v.GetVoter(c, id)
+
+	c.JSON(http.StatusOK, voter)
+}
+
+func (v *VoterApi) ListPollHistory(c *gin.Context) {
+	id := getIDFromContext(c, "id")
+	voter := v.GetVoter(c, id)
+	voteHistory := voter.GetVoteHistory()
+
+	c.JSON(http.StatusOK, voteHistory)
+}
+
+func (v *VoterApi) ListSinglePollData(c *gin.Context) {
+	id := getIDFromContext(c, "id")
+	pollId := getIDFromContext(c, "pollid")
+
+	voter := v.GetVoter(c, id)
+	pollData := voter.GetPollById(pollId)
+
+	c.JSON(http.StatusOK, pollData)
+}
+
+func (v *VoterApi) AddPollData(c *gin.Context) {
+	id := getIDFromContext(c, "id")
+	pollId := getIDFromContext(c, "pollid")
+
+	voter := v.GetVoter(c, id)
+	voter.AddPoll(pollId)
+}
+
+func (v *VoterApi) GetHealth(c *gin.Context) {
+	c.JSON(http.StatusOK, "API is running")
+}
+
+func getIDFromContext(c *gin.Context, param string) uint {
+	idString := c.Param(param)
+	id, err := strconv.ParseInt(idString, 10, 32)
+	if err != nil {
+		log.Println("Error converting id to integer: ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	return uint(id)
+}
---
+++ Final-Project/voter/voter.go
@@ -0,0 +1,162 @@
+package voter
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/nitishm/go-rejson/v4"
+)
+
+type voterPoll struct {
+	PollID   uint
+	VoteDate time.Time
+}
+
+type Voter struct {
+	VoterID     uint        `json:"voterID"`
+	FirstName   string      `json:"firstName"`
+	LastName    string      `json:"lastName"`
+	VoteHistory []voterPoll `json:"voteHistory"`
+}
+
+const (
+	RedisNilError        = "redis: nil"
+	RedisDefaultLocation = "0.0.0.0:6379"
+	RedisKeyPrefix       = "voter:"
+)
+
+type cache struct {
+	cacheClient *redis.Client
+	jsonHelper  *rejson.Handler
+	context     context.Context
+}
+
+type VoterList struct {
+	cache
+}
+
+// constructor for VoterList struct
+func NewVoterList() *VoterList {
+	redisUrl := os.Getenv("REDIS_URL")
+	if redisUrl == "" {
+		redisUrl = RedisDefaultLocation
+	}
+	voterList := NewWithCacheInstance(redisUrl)
+
+	return voterList
+}
+
+func NewWithCacheInstance(location string) *VoterList {
+	client := redis.NewClient(&redis.Options{
+		Addr: location,
+	})
+
+	ctx := context.Background()
+
+	err := client.Ping(ctx).Err()
+	if err != nil {
+		log.Println("Error connecting to redis" + err.Error())
+		os.Exit(1)
+	}
+
+	jsonHelper := rejson.NewReJSONHandler()
+	jsonHelper.SetGoRedisClientWithContext(ctx, client)
+
+	return &VoterList{
+		cache: cache{
+			cacheClient: client,
+			jsonHelper:  jsonHelper,
+			context:     ctx,
+		},
+	}
+}
+
+func NewVoter(id uint, fn, ln string) *Voter {
+	return &Voter{
+		FirstName:   fn,
+		LastName:    ln,
+		VoteHistory: []voterPoll{},
+	}
+}
+
+func NewSampleVoter() *Voter {
+	return &Voter{
+		VoterID:   1,
+		FirstName: "John",
+		LastName:  "Doe",
+		VoteHistory: []voterPoll{
+			{PollID: 1, VoteDate: time.Now()},
+		},
+	}
+}
+
+func (v *Voter) AddPoll(pollID uint) {
+	v.VoteHistory = append(v.VoteHistory, voterPoll{PollID: pollID, VoteDate: time.Now()})
+}
+
+func (v *Voter) AddPollWithTimeDetails(pollID uint, timeOfPoll time.Time) {
+	v.VoteHistory = append(v.VoteHistory, voterPoll{PollID: pollID, VoteDate: timeOfPoll})
+}
+
+func (v *Voter) ToJson() string {
+	b, _ := json.Marshal(v)
+	return string(b)
+}
+
+func (vl *VoterList) AddVoter(voterID uint, newVoter Voter) error {
+	redisKey := redisKeyFromId(newVoter.VoterID)
+	var existingVoter Voter
+	if err := vl.getVoterFromRedis(redisKey, &existingVoter); err == nil {
+		return errors.New("voter with that ID already exists")
+	}
+	if _, err := vl.jsonHelper.JSONSet(redisKey, ".", newVoter); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (vl *VoterList) GetVoter(id uint) (*Voter, error) {
+	var voter Voter
+	pattern := redisKeyFromId(id)
+	err := vl.getVoterFromRedis(pattern, &voter)
+	if err != nil {
+		return &Voter{}, err
+	}
+	return &voter, nil
+}
+
+func (v *Voter) GetVoteHistory() []time.Time {
+	var pollDates []time.Time
+	for _, history := range v.VoteHistory {
+		pollDates = append(pollDates, history.VoteDate)
+	}
+
+	return pollDates
+}
+
+func (v *Voter) GetPollById(pollId uint) time.Time {
+	return v.VoteHistory[pollId].VoteDate
+}
+
+func redisKeyFromId(id uint) string {
+	return fmt.Sprintf("%s%d", RedisKeyPrefix, id)
+}
+
+func (vl *VoterList) getVoterFromRedis(key string, voter *Voter) error {
+	voterObject, err := vl.jsonHelper.JSONGet(key, ".")
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(voterObject.([]byte), voter)
+	if err != nil {
+		return err
+	}
+	return nil
+}
