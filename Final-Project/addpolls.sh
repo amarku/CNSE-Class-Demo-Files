69750a57c7175bb5d41f70307136f5b7002d1e2f
Andrew Marku <andrewmarku@gmail.com>
2023-09-04 18:29:46 -0400

    script for adding polls

---
+++ Final-Project/addpolls.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -d '{ "pollID": 1, "pollTitle": "Favorite Pet", "pollQuestion": "What type of pet do you like best?", "pollOptions": [
+{"pollOptionID": 1, "pollOptionValue": "Dog"},
+{"pollOptionID": 2, "pollOptionValue": "Cat"},
+{"pollOptionID": 3, "pollOptionValue": "Fish"},
+{"pollOptionID": 4, "pollOptionValue": "Bird"},
+{"pollOptionID": 5, "pollOptionValue": "NONE"}
+]}' -H "Content-Type: application/json" -X POST http://localhost:2080/polls
