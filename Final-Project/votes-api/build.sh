228404e36ea4e214981cabe1a43cd78384521ca4
Andrew Marku <andrewmarku@gmail.com>
2023-09-04 22:23:43 -0400

    set up votes api

---
+++ Final-Project/votes-api/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build --tag votes-api:v1 -f ./Dockerfile .
---
+++ Final-Project/votes-api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"votes-api/api"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	votesApi := api.NewVotesApi()
+
+	r.GET("/votes", votesApi.ListAllVotes)
+	r.GET("/votes/:id", votesApi.ListVote)
+	r.POST("/votes", votesApi.AddVote)
+
+	serverPath := "0.0.0.0:3080"
+	r.Run(serverPath)
+}
