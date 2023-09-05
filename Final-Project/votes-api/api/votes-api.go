2c05de0b36258a82d99be721c5342bdc38468a7e
Andrew Marku <andrewmarku@gmail.com>
2023-09-04 22:23:43 -0400

    set up votes api

---
+++ Final-Project/votes-api/api/votes-api.go
@@ -0,0 +1,78 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+	"votes-api/schema"
+)
+
+type VoteApi struct {
+	voteList *schema.VoteList
+}
+
+func NewVotesApi() *VoteApi {
+	return &VoteApi{
+		schema.NewVoteList(),
+	}
+}
+
+func (p *VoteApi) AddVote(c *gin.Context) {
+	var newVote schema.Vote
+
+	if err := c.ShouldBindJSON(&newVote); err != nil {
+		log.Println("Error binding JSON: ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	if err := p.voteList.AddVote(newVote); err != nil {
+		log.Println("Error adding vote: ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	c.JSON(http.StatusOK, newVote)
+}
+
+func (p *VoteApi) GetVote(c *gin.Context, voteID uint) *schema.Vote {
+	vote, err := p.voteList.GetVote(voteID)
+	if err != nil {
+		log.Println("error getting vote: ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return nil
+	}
+	return vote
+}
+
+func (p *VoteApi) ListAllVotes(c *gin.Context) {
+	voteList, err := p.voteList.GetAllVotes()
+	if err != nil {
+		log.Println("Error getting all vote: ", err)
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+
+	if voteList == nil {
+		voteList = make([]schema.Vote, 0)
+	}
+	c.JSON(http.StatusOK, voteList)
+}
+
+func (p *VoteApi) ListVote(c *gin.Context) {
+	id := getIDFromContext(c, "id")
+	vote := p.GetVote(c, id)
+
+	c.JSON(http.StatusOK, vote)
+}
+
+func getIDFromContext(c *gin.Context, param string) uint {
+	idString := c.Param(param)
+	id, err := strconv.ParseInt(idString, 10, 32)
+	if err != nil {
+		log.Println("Error converting id to integer: ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	return uint(id)
+}
---
+++ Final-Project/votes-api/schema/votes.go
@@ -0,0 +1,161 @@
+package schema
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/nitishm/go-rejson/v4"
+	"log"
+	"os"
+	"sort"
+)
+
+type Vote struct {
+	VoteID    uint
+	VoterID   uint
+	PollID    uint
+	VoteValue uint
+}
+type VoteData struct {
+	Votes []Vote //A map of VoterIDs as keys and Voter structs as values
+}
+
+func NewVote(pid, vid, vtrid, vval uint) *Vote {
+	return &Vote{
+		VoteID:    vid,
+		VoterID:   vtrid,
+		PollID:    pid,
+		VoteValue: vval,
+	}
+}
+
+func NewSampleVote() *Vote {
+	return &Vote{
+		VoteID:    1,
+		PollID:    1,
+		VoterID:   1,
+		VoteValue: 1,
+	}
+}
+
+func (p *Vote) ToJson() string {
+	b, _ := json.Marshal(p)
+	return string(b)
+}
+
+const (
+	RedisNilError        = "redis: nil"
+	RedisKeyPrefix       = "vote:"
+	RedisDefaultLocation = "0.0.0.0:6379"
+)
+
+type cache struct {
+	cacheClient *redis.Client
+	jsonHelper  *rejson.Handler
+	context     context.Context
+}
+
+type VoteList struct {
+	cache
+}
+
+func NewVoteList() *VoteList {
+	redisUrl := os.Getenv("REDIS_URL")
+	if redisUrl == "" {
+		redisUrl = RedisDefaultLocation
+	}
+	voteList := NewWithCacheInstance(redisUrl)
+
+	return voteList
+}
+
+func NewWithCacheInstance(location string) *VoteList {
+	client := redis.NewClient(&redis.Options{
+		Addr: location,
+	})
+
+	ctx := context.Background()
+
+	err := client.Ping(ctx).Err()
+	if err != nil {
+		log.Println("Error connecting to redis" + err.Error())
+		os.Exit(1)
+	}
+
+	jsonHelper := rejson.NewReJSONHandler()
+	jsonHelper.SetGoRedisClientWithContext(ctx, client)
+
+	return &VoteList{
+		cache: cache{
+			cacheClient: client,
+			jsonHelper:  jsonHelper,
+			context:     ctx,
+		},
+	}
+}
+
+func (vl *VoteList) AddVote(newVote Vote) error {
+	redisKey := redisKeyFromId(newVote.VoteID)
+	var existingVote Vote
+	if err := vl.getVoteFromRedis(redisKey, &existingVote); err == nil {
+		return errors.New("vote with that ID already exists")
+	}
+	if _, err := vl.jsonHelper.JSONSet(redisKey, ".", newVote); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (vl *VoteList) GetVote(id uint) (*Vote, error) {
+	var vote Vote
+	pattern := redisKeyFromId(id)
+	err := vl.getVoteFromRedis(pattern, &vote)
+	if err != nil {
+		return &Vote{}, err
+	}
+	return &vote, nil
+}
+
+func (vl *VoteList) GetAllVotes() ([]Vote, error) {
+	var voteList []Vote
+	var vote Vote
+
+	pattern := RedisKeyPrefix + "*"
+	ks, err := vl.cacheClient.Keys(vl.context, pattern).Result()
+	if err != nil {
+		return nil, err
+	}
+	for _, key := range ks {
+		log.Println(key)
+		err = vl.getVoteFromRedis(key, &vote)
+		if err != nil {
+			return nil, err
+		}
+		voteList = append(voteList, vote)
+	}
+
+	sort.Slice(voteList, func(i, j int) bool {
+		return voteList[i].VoteID < voteList[j].VoteID
+	})
+
+	return voteList, nil
+}
+
+func redisKeyFromId(id uint) string {
+	return fmt.Sprintf("%s%d", RedisKeyPrefix, id)
+}
+
+func (vl *VoteList) getVoteFromRedis(key string, Vote *Vote) error {
+	voteObject, err := vl.jsonHelper.JSONGet(key, ".")
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(voteObject.([]byte), Vote)
+	if err != nil {
+		return err
+	}
+	return nil
+}
